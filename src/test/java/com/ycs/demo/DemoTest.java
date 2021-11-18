e1bb2051ce702698c20f3d8f3b1c291206fcad17
sai <talanki.sai561@gmail.com>
2021-11-18 22:08:43 +0530

    DemoTestcommit

---
+++ src/test/java/com/ycs/demo/DemoTest.java
@@ -0,0 +1,19 @@
+package com.ycs.demo;
+
+class Practice{
+	public void m1() {
+		System.out.println("m1-A");
+	}
+	public void m2() {
+		System.out.println("m2-A");
+	}
+	
+}
+public class DemoTest {
+	public static void main(String[] args) {
+		Practice pes=new Practice();
+		pes.m1();
+		pes.m2();
+	}
+
+}
